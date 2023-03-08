1b4fdcbe07e1580477296d81136bd3cd5bbebb5d
Nick Dellar <ndelfern@live.ca>
2023-03-08 15:33:19 -0500

    change database tables

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 const handleRefreshToken = (req, res) => {
   const cookies = req.cookies;
-  if (!cookies?.jwt)
+  if (!cookies)
     return res
       .sendStatus(401);
   const refreshToken = cookies.jwt;
