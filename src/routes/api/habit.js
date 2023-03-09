0cdb3007fde8f18a8c7199d836c4c2fa3f0bf028
MackyClap <martimac2320@gmail.com>
2023-03-09 10:36:15 -0500

    send res.json habits from query

--- src/routes/api/habit.js
+++ src/routes/api/habit.js
@@ -3,19 +3,18 @@ const { response } = require("express");
 const router = require("express").Router();
 
 
-// module.exports = db => {
+module.exports = db => {
 
   router.get("/", (req, res) => {
-    // db.query(
-    //   `
-    //   SELECT * from habits;
-    //   `
-    // )
-    //   .then(({ rows: habits}) => {
-    //     response.json(habits)
-    //   })
-    res.send("Hello World")
+    db.query(
+      `
+      SELECT * from habits;
+      `
+      )
+      .then(({ rows: habits}) => {
+        res.json(habits)
+        res.send(res.json(habits))
+      })
   })
-// }
-
-module.exports = router;
+  return router
+}
