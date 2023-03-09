bbedd892e883d164d744a85a3655b8345d2a3f00
MackyClap <martimac2320@gmail.com>
2023-03-09 13:04:10 -0500

    review create habit functioin and post route to ensure data is transferring

--- controllers/habitController.js
+++ controllers/habitController.js
@@ -1,5 +1,22 @@
-const addHabit = (db) => {
-  const queryParams = []
+const router = require("express").Router();
+const db = require("../src/db");
+
+const getHabits = () => {
+  return db
+  .query(
+    `
+    SELECT * from habits;
+    `
+    )
+    .then((results) => {
+      console.log("RESULTS:", results.rows)
+      return results.rows
+    });
+};
+
+const addHabits = (habitBody) => {
+  const {unique_event_id, title, body, start_date, end_date, start_time, end_time, days, user_id, completed} = habitBody
+  const queryParams = [unique_event_id, title, body, start_date, end_date, start_time, end_time, days, user_id, completed]
   const queryString =
     `
       INSERT INTO habits 
@@ -8,8 +25,7 @@ const addHabit = (db) => {
       RETURNING *
     `
   
-  return db
-    .query(queryString, queryParams)
+   return db.query(queryString, queryParams)
     .then((results) => {
       return results.rows
     })
@@ -18,4 +34,6 @@ const addHabit = (db) => {
     })
 };
 
-module.exports = {addHabit};
+module.exports = {
+  addHabits,
+   getHabits};
--- src/routes/api/habit.js
+++ src/routes/api/habit.js
@@ -2,23 +2,27 @@ const router = require("express").Router();
 const habitController = require('../../../controllers/habitController')
 
 
-module.exports = db => {
+module.exports = (db) => {
 
   router.get("/", (req, res) => {
-    db.query(
-      `
-      SELECT * from habits;
-      `
-      )
-      .then(({ rows: habits}) => {
-        res.json(habits)
-        res.send(res.json(habits))
-      });
+    habitController.getHabits()
+      .then(habits => res.send(habits))
+      // .then(res.send("hello"))
+    ///Working code
+    // db.query(
+    //   `
+    //   SELECT * from habits;
+    //   `
+    //   )
+    //   .then(({ rows: habits}) => {
+    //     res.json(habits)
+    //     res.send(res.json(habits))
+    //   });
   });
 
   router.post("/", (req,res) => {
 
-    habitController.addHabit({...req.body})
+    habitController.addHabits({...req.body})
       .then(habit => {
         res.send(habit);
       })
