8074d0369820d7fb2c50cfdc16a7981f877db3ee
MackyClap <martimac2320@gmail.com>
2023-03-09 11:26:41 -0500

    move create logic to controller and create post route

--- controllers/habitController.js
+++ controllers/habitController.js
@@ -1,7 +1,21 @@
-// module.exports = db => {
-//   db.query(
-//     `
-//       SELECT * FROM habits
-//     `
-//   )
-// }
+const addHabit = (db) => {
+  const queryParams = []
+  const queryString =
+    `
+      INSERT INTO habits 
+      (unique_event_id, title, body, start_date, end_date, start_time, end_time, days, user_id, completed)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+      RETURNING *
+    `
+  
+  return db
+    .query(queryString, queryParams)
+    .then((results) => {
+      return results.rows
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
+};
+
+module.exports = {addHabit};
--- src/routes/api/habit.js
+++ src/routes/api/habit.js
@@ -1,6 +1,5 @@
-const { response } = require("express");
-
 const router = require("express").Router();
+const habitController = require('../../../controllers/habitController')
 
 
 module.exports = db => {
@@ -14,9 +13,20 @@ module.exports = db => {
       .then(({ rows: habits}) => {
         res.json(habits)
         res.send(res.json(habits))
+      });
+  });
+
+  router.post("/", (req,res) => {
+
+    habitController.addHabit({...req.body})
+      .then(habit => {
+        res.send(habit);
       })
-  })
+      .catch(err => {
+        console.error(err);
+        res.send(err)
+      });
+  });
+  
   return router
 }
-
-// module.exports = router;
