da41f62d59203840e36db1893719115398d96fff
MackyClap <martimac2320@gmail.com>
2023-03-10 12:28:02 -0500

    insert seed data for events

--- src/db/schema/development.sql
+++ src/db/schema/development.sql
@@ -10,3 +10,9 @@ INSERT INTO habits (unique_event_id, title, body, start_date, end_date, start_ti
   ('running20230309', 'Running', 'I want to run 3 times per week', '2023-03-09', '2023-03-28', '07:00', '08:00', 'Monday,Wednesday' , 2, false),
   ('coding20230309', 'Coding',  ' Practicing everyday to be a great developer', '2023-03-09', '2023-03-28', '18:00', '22:00', 'Monday,Tuesday' , 1, false ),
   ('coding20230319', 'Coding',  ' Practicing everyday to be a great developer', '2023-03-09', '2023-03-28', '23:00', '24:00', 'Thursday,Friday', 3, false );
+
+INSERT INTO events (unique_event_id, habits_id)
+  VALUES
+  ('111', 1)
+  ('999', 2)
+  ('555', 3)
