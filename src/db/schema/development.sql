1ce060baf730da6494f7b82d950124f0b862dbf8
MackyClap <martimac2320@gmail.com>
2023-03-09 22:23:37 -0500

    ensure all times are military format

--- src/db/schema/development.sql
+++ src/db/schema/development.sql
@@ -7,6 +7,6 @@ INSERT INTO users (avatar, first_name, last_name, username, email, password)
 
 INSERT INTO habits (unique_event_id, title, body, start_date, end_date, start_time, end_time, days, user_id, completed)
   VALUES
-  ('running20230309', 'Running', 'I want to run 3 times per week', '2023-03-09', '2023-03-28', '7:00', '8:00', 'Monday,Wednesday' , 2, false),
+  ('running20230309', 'Running', 'I want to run 3 times per week', '2023-03-09', '2023-03-28', '07:00', '08:00', 'Monday,Wednesday' , 2, false),
   ('coding20230309', 'Coding',  ' Practicing everyday to be a great developer', '2023-03-09', '2023-03-28', '18:00', '22:00', 'Monday,Tuesday' , 1, false ),
   ('coding20230319', 'Coding',  ' Practicing everyday to be a great developer', '2023-03-09', '2023-03-28', '23:00', '24:00', 'Thursday,Friday', 3, false );
