60590ca697cd851fc7fe252474faf5533501f438
Nick Dellar <ndelfern@live.ca>
2023-03-11 21:51:40 -0500

    change res.send error to throw error

--- controllers/habitController.js
+++ controllers/habitController.js
@@ -23,28 +23,21 @@ const getHabits = async (req, res) => {
 
 const addHabits = async (habitBody) => {
   try {
-    const {title, body, start_date, end_date, start_time, end_time, days, user_id, completed} = habitBody
-    const queryParams = [title, body, start_date, end_date, start_time, end_time, days, user_id, completed]
-    const queryString =
-      `
-        INSERT INTO habits 
-        (title, body, start_date, end_date, start_time, end_time, days, user_id, completed)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-        RETURNING *
-      `
+    const { unique_event_id, habit_id, user_id, completed } = habitBody
+    const queryParams = [unique_event_id, habit_id, user_id, completed]
+    const queryString = 
+    `
+      INSERT INTO events 
+      (unique_event_id, habit_id, user_id, completed)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *
+    `
     const results = await db.query(queryString, queryParams);
     return results.rows;
   } catch (error) {
     console.error(error);
-    res.sendStatus(500);
+    throw new Error("Failed to add event"); // throw an error instead of sending 500 status code
   }
-  return db.query(queryString, queryParams)
-  .then((results) => {
-    return results.rows
-  })
-  .catch((err) => {
-    console.log(err.message)
-  })
 };
 
 const getEvents = async (req, res) => {
@@ -82,7 +75,7 @@ const addEvent = async (eventBody) => {
     return results.rows;
   } catch (error) {
     console.error(error);
-    res.sendStatus(500);
+    throw new Error("Failed to add event"); // throw an error instead of sending 500 status code
   }
 };
 
